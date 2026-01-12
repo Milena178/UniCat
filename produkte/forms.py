dfe56e3f63a2d7e0ad16511f4f00ccf3c37952a9
Milena Malottki <mima4784@bht-berlin.de>
2026-01-12 15:21:58 +0100

    bei produkte models die Beschriftung geändert

--- produkte/forms.py
+++ produkte/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import Product
+from .models import Produkte
 
 class ProductForm(forms.ModelForm):
     class Meta:
-        model = Product
+        model = Produkte
         fields = [
             "name",
             "beschreibung",
