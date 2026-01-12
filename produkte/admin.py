563d2497ca15000b6e3656fb35b81de6d4a0d705
Carina <carina.jahnke@proton.me>
2026-01-12 15:59:29 +0100

    Testprodukt

--- produkte/admin.py
+++ produkte/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Produkte
 
-# Register your models here.
+@admin.register(Produkte)
+class ProdukteAdmin(admin.ModelAdmin):
+    list_display = ['name', 'mindestpreis', 'anzahlListungen', 'erstelltAm', 'istArchiviert']
+    list_filter = ['istArchiviert', 'erstelltAm']
+    search_fields = ['name', 'beschreibung']
+    readonly_fields = ['erstelltAm']
